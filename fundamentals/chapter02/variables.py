e06c65765e80576ed45ef89ae96d31a8a7d38d73
Lhakpa Sonam Sherpa <soamsrp.653@gmail.com>
2026-01-06 00:00:11 +0545

    Learn : Method for title casing string

---
+++ fundamentals/chapter02/variables.py
@@ -0,0 +1,10 @@
+message = "Hello Python world!"
+# print(mesage) gives error
+print(message)
+
+# Changing case in a string with methods
+
+name = "ada lovelance"
+print(name.title()) # returns Ada Lovelance title->Title case
+
+
