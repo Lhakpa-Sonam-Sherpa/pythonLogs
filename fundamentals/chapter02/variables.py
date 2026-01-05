2a8bbf863ef05ed0be26984d33627517db21c87c
Lhakpa Sonam Sherpa <soamsrp.653@gmail.com>
2026-01-06 00:20:04 +0545

    Add : String formatting methods & f-strings

--- fundamentals/chapter02/variables.py
+++ fundamentals/chapter02/variables.py
@@ -2,9 +2,24 @@ message = "Hello Python world!"
 # print(mesage) gives error
 print(message)
 
-# Changing case in a string with methods
+# String formatting
 
 name = "ada lovelance"
-print(name.title()) # returns Ada Lovelance title->Title case
+#case formatting string methods()
+print(name.title()) # returns Ada Lovelance title->Title Case
+print(name.upper()) # returns ADA LOVELANCE upper->UPPER CASE
+print(name.lower()) # returns ADA LOVELANCE lower->lower case
 
+# Using variables in string
+first_name = "albert"
+last_name = "einstein"
+
+full_name = f"{first_name} {last_name}" 
+# f is for 'fromat'
+# f"..{variable}..." is a format string
+
+full_name = full_name.title() #title case the name
+
+message = f"Hello, {full_name}"
+print(message)
 
